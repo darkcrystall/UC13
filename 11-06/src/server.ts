8471e9dc5157d8789739520175f7d6452e51f5fc
Eduarda C. <dossantosec@outlook.com>
2026-06-16 16:15:34 -0300

    feat: novo exemplo de rota

--- 11-06/src/server.ts
+++ 11-06/src/server.ts
@@ -11,7 +11,7 @@ app.post("/livros", async (req: Request, res: Response): Promise<Response> => {
       "INSERT INTO livros (titulo, autor, ano_lancamento) VALUES (?, ?, ?);",
       [titulo, autor, ano_lancamento]
     );
-      return res.status(201).json("Livro cadastrado com sucesso.");
+    return res.status(201).json("Livro cadastrado com sucesso.");
   } catch (error) {
     return res.status(500).json("Erro ao cadastrar livro: " + error);
   }
@@ -36,9 +36,34 @@ app.put(
         "UPDATE livros SET titulo = ?, autor = ?, ano_lancamento = ? WHERE id = ?;",
         [titulo, autor, ano_lancamento, id]
       );
-    return res.status(200).json("Livro atualizado com sucesso.");
+      return res.status(200).json("Livro atualizado com sucesso.");
     } catch (error) {
       return res.status(500).json("Erro ao atualizar o livro: " + error);
+    }
+  }
+);
+// patch: UPDATE (parcial)
+app.patch(
+  "/livros/:id",
+  async (req: Request, res: Response): Promise<Response> => {
+    try {
+      const { id } = req.params;
+      const campos = req.body;
+      //  monta dinamicamente os arrays
+      const keys = Object.keys(campos); // pega as chaves do objeto
+      const values = Object.values(campos); // pega os valores do objeto
+      if (keys.length === 0) {
+        return res.status(400).json("Nenhum campo enviado para atualização");
+      }
+      const setClause = keys.map((key) => `${key} = ?`).join(", ");
+      /* titulo = ?, autor = ?, ano_lancamento = ?*/
+      await pool.query(`UPDATE livros SET ${setClause} WHERE id = ?`, [
+        ...values,
+        id,
+      ]);
+      return res.status(200).json("Livro atualizado parcialmente com sucesso");
+    } catch (error) {
+      return res.status(500).json("Erro interno do servidor: " + error);
     }
   }
 );
@@ -48,10 +73,7 @@ app.delete(
   async (req: Request, res: Response): Promise<Response> => {
     try {
       const { id } = req.params;
-      await pool.query(
-        "DELETE FROM livros WHERE id = ?;",
-        [id]
-      );
+      await pool.query("DELETE FROM livros WHERE id = ?;", [id]);
       return res.status(200).json("Livro deletado com sucesso.");
     } catch (error) {
       return res.status(500).json("Erro ao deletar o livro: " + error);
