42a45da53e9f68532f608d5e594c00e0552e9326
Eduarda C. <dossantosec@outlook.com>
2026-06-26 01:55:11 -0300

    feat: melhora exemplo

--- 23-06/src/controllers/UserController.ts
+++ 23-06/src/controllers/UserController.ts
@@ -2,14 +2,16 @@ import { Response, Request } from "express";
 import { UserService } from "../services/UserService";
 import { AppError } from "../errors/error-handler";
 export class UserController {
-  private service: UserService;
-  async getAll(req: Request, res: Response): Promise<Response | undefined> {
+  private service: UserService = new UserService();
+  async getAll(req: Request, res: Response): Promise<Response> {
     try {
       const users = await this.service.getAllUsers();
       return res.status(200).json({ users });     
     } catch (error) {
       if (error instanceof AppError) {
         return res.status(error.statusCode).json({ message: error.message });
+      } else {
+        return res.status(500).json({ message: "Erro inesperado" });
       }
     }
   }
