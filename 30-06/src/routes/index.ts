3a4e31a07afe9258a684cc128b495f5bb554ae30
Eduarda C. <dossantosec@outlook.com>
2026-07-05 01:27:25 -0300

    feat: validação de id

--- 30-06/src/routes/index.ts
+++ 30-06/src/routes/index.ts
@@ -1,19 +1,34 @@
 import { Router } from "express";
 import { UserController } from "../controllers/UserController";
 import { validateUser } from "../middlewares/validateUser";
-import { validateUserV2 } from "../middlewares/validateUserv2";
+import { validateUserFields } from "../middlewares/validateUserFields";
+import { validateId } from "../middlewares/validateId";
 export const router = Router(); // cria o objeto das rotas do express (necessário para criar as rotas)
 const userController = new UserController(); // objeto da classe UserController
 // USER ROUTES
 // para criar uma rota, usamos o objeto router e passamos como parâmetroso caminho e o objeto do controlador que vai ser executado, e os middlewares, se necessário
 // .bind(userController) garante que o "this" dentro do método continue apontando pra instância certa quando o Express chamar essa função
 router.get("/users", userController.list.bind(userController));
-router.get("/users/:id", userController.getById.bind(userController));
+router.get(
+  "/users/:id",
+  validateId,
+  userController.getById.bind(userController)
+);
 // validateUser roda primeiro: se os dados estiverem inválidos, a requisição já é interrompida ali, sem nem chegar ao Controller
-router.post("/users", validateUserV2, userController.create.bind(userController));
+router.post(
+  "/users",
+  validateUser,
+  validateUserFields,
+  userController.create.bind(userController)
+);
 router.put(
   "/users/:id",
-  validateUserV2,
+  validateId,
+  validateUserFields,
   userController.update.bind(userController)
 );
-router.delete("/users/:id", userController.delete.bind(userController));
+router.delete(
+  "/users/:id",
+  validateId,
+  userController.delete.bind(userController)
+);
--- 30-06/src/schemas/UserSchema.ts
+++ 30-06/src/schemas/UserSchema.ts
@@ -1,14 +1,7 @@
 import { z } from "zod";
-
-export const updateUserSchema = z.object({
+export const UserSchema = z.object({
   name: z.string().trim().min(1, "O nome não pode ser vazio").optional(),
-
-  email: z
-    .string()
-    .trim()
-    .email("Email inválido")
-    .optional(),
-
+  email: z.string().trim().email("Email inválido").optional(),
   password: z
     .string()
     .min(6, "A senha deve ter pelo menos 6 caracteres")
