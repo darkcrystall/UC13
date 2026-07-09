835a9f3c23ef4c84b47c5ef538be91d15d87423f
Eduarda C. <dossantosec@outlook.com>
2026-07-09 16:09:00 -0300

    feat: jwt no exercício

---
+++ 07-07/src/utils/jwt.ts
@@ -0,0 +1,16 @@
+import * as env from "dotenv";
+import jwt from "jsonwebtoken";
+import { Payload } from "../interfaces/IPayload";
+env.config();
+export function generateToken(payload: Payload) {
+  return jwt.sign(payload, process.env.JWT_SECRET!, {
+    expiresIn: Number(process.env.JWT_EXPIRES_IN),
+  });
+}
+export function verifyToken(token: string) {
+  try {
+    return jwt.verify(token, process.env.JWT_SECRET!);
+  } catch (error) {
+    return null;
+  }
+}
