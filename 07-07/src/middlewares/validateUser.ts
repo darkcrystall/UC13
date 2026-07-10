a534c33bade80a13f8bfdd7d3e0b81ac0a35a35b
Eduarda C. <dossantosec@outlook.com>
2026-07-10 16:58:10 -0300

    feat: melhora exercício

--- 07-07/src/middlewares/validateUser.ts
+++ 07-07/src/middlewares/validateUser.ts
@@ -1,15 +1,25 @@
 import { NextFunction, Request, Response } from "express";
 import { createUserSchema, updateUserSchema } from "../schemas/user.schema";
 import { BadRequestError } from "../errors/BadRequestError";
-export function validateUserCreate(req: Request, res: Response, next: NextFunction) {
-    const result = createUserSchema.safeParse(req.body);
-    if (!result.success) {
-        throw new BadRequestError(`${result.error}`)
-    }
+export function validateUserCreate(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const result = createUserSchema.safeParse(req.body);
+  if (!result.success) {
+    throw new BadRequestError(`${result.error}`);
+  }
+  next();
 }
-export function validateUserUpdate(req: Request, res: Response, next: NextFunction) {
-    const result = updateUserSchema.safeParse(req.body);
-    if (!result.success) {
-        throw new BadRequestError(`${result.error}`)
-    }
+export function validateUserUpdate(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const result = updateUserSchema.safeParse(req.body);
+  if (!result.success) {
+    throw new BadRequestError(`${result.error}`);
+  }
+  next();
 }
